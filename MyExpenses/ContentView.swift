25bd4a865043ae478d966a2b358fca740a0296bc
denvolkov <denvolkov@inbox.ru>
2021-01-12 15:34:27 +0300

    Added onDelete

--- MyExpenses/ContentView.swift
+++ MyExpenses/ContentView.swift
@@ -2,17 +2,56 @@
 //  ContentView.swift
 //  MyExpenses
 //
-//  Created by test on 12.01.2021.
+//  Created by Denis Volkov on 12.01.2021.
 //
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("Мои расходы")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        let expense = ExpenseItem(name: "Test", type: "", amount: 50)
+                                        self.expenses.items.append(expense)
+                                    }) {
+                                        Image(systemName: "plus")
+                                    }
+            )
+        }
+        
+        }
+    
+    func removeItems(as offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
