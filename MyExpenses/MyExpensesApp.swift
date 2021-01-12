e02a24a9f74389508a5c5075c0d0efc0e7d2ee65
denvolkov <denvolkov@inbox.ru>
2021-01-12 10:24:53 +0300

    Initial Commit

---
+++ MyExpenses/MyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesApp.swift
+//  MyExpenses
+//
+//  Created by test on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
